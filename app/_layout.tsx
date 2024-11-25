a774765aaf245a87baf609251a3f7d233e151dd4
Rohithegade02 <yournew@email.com>
2024-11-26 00:49:05 +0530

    wip

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,41 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import tokenCache from '@/utils/cache'
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+import {  Slot } from 'expo-router'
+import {DMSans_400Regular, DMSans_500Medium, DMSans_700Bold, useFonts} from '@expo-google-fonts/dm-sans'
+import { useEffect } from 'react'
+import * as SplashScreen from 'expo-splash-screen'
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+//env data
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+  )
+}
+SplashScreen.preventAutoHideAsync()
+const InitialLayout = () => {
+  const [ fontsLoaded ] = useFonts({
+    DMSans_400Regular,
+    DMSans_500Medium,
+    DMSans_700Bold
+  })
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
     }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+  }, [fontsLoaded])
+  
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+   <Slot/>
+  )
+}
+export default function RootLayoutNav() {
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ClerkLoaded>
+        <InitialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
+  )
 }
