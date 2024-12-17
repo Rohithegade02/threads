7cd9f9ba9f34b66b2924e0abc1174412d2958eff
Rohithegade02 <rohithegade8@gmail.com>
2024-12-17 20:44:04 +0530

    removed android

--- utils/dateTime.ts
+++ utils/dateTime.ts
@@ -3,3 +3,5 @@ import { formatDistanceToNow } from 'date-fns'
 export function formatTime(creationTime: string | number | Date) {
   return formatDistanceToNow(new Date(creationTime), { addSuffix: true })
 }
+
+//https://threads-d66e5.firebaseapp.com/__/auth/handler
